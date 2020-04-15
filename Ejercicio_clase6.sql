a4796f3a221e16f6b65fbef3e94acee9e4fc01a8
IgnaCat <itsigna02@gmail.com>
2020-04-15 13:45:02 -0300

    Exercises 5 to 8 class 6

--- Ejercicio_clase6.sql
+++ Ejercicio_clase6.sql
@@ -9,7 +9,55 @@ select first_name, last_name
    order by last_name;
    
   
- #5
+#5
+select first_name, last_name
+from actor 
+where actor_id in (select actor.actor_id 
+ 					from actor, film f, film_actor fa
+ 					where actor.actor_id = fa.actor_id 
+ 					and fa.film_id = f.film_id 
+ 					and f.title in ('BETRAYED REAR', 'CATCH AMISTAD'));
+ 				
+ 			
+#6
+select first_name, last_name
+from actor 
+where actor_id in (select actor.actor_id 
+ 					from actor, film f, film_actor fa
+ 					where actor.actor_id = fa.actor_id 
+ 					and fa.film_id = f.film_id 
+ 					and f.title = 'BETRAYED REAR')
+and actor_id not in (select actor.actor_id 
+ 					from actor, film f, film_actor fa
+ 					where actor.actor_id = fa.actor_id 
+ 					and fa.film_id = f.film_id 
+ 					and f.title = 'CATCH AMISTAD');
+ 				
+ 				
+#7
+select first_name, last_name
+from actor 
+where actor_id in (select actor.actor_id 
+ 					from actor, film f, film_actor fa
+ 					where actor.actor_id = fa.actor_id 
+ 					and fa.film_id = f.film_id 
+ 					and f.title = 'BETRAYED REAR')
+and actor_id in (select actor.actor_id 
+ 					from actor, film f, film_actor fa
+ 					where actor.actor_id = fa.actor_id 
+ 					and fa.film_id = f.film_id 
+ 					and f.title = 'CATCH AMISTAD');
+ 				
+ 				
+#8
+select first_name, last_name
+from actor 
+where actor_id not in (select actor.actor_id 
+ 					from actor, film f, film_actor fa
+ 					where actor.actor_id = fa.actor_id 
+ 					and fa.film_id = f.film_id 
+ 					and f.title in ('BETRAYED REAR', 'CATCH AMISTAD'));
+
  
 
 
