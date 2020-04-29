a16973802b96baa5031abdf71cdcb9464d821c4f
IgnaCat <itsigna02@gmail.com>
2020-04-29 18:34:09 -0300

    Correction of class 6 and i add exercises 3, 4 class 7

--- Ejercicio_clase6.sql
+++ Ejercicio_clase6.sql
@@ -12,10 +12,9 @@ select first_name, last_name
 #2
 select first_name, last_name
 from actor a
-where EXISTS (select fa.actor_id 
+where NOT EXISTS (select fa.actor_id 
 			from film_actor fa 
-			where fa.actor_id = a.actor_id 
-			and fa.film_id is null);
+			where fa.actor_id = a.actor_id);
 		
 		
 #3
@@ -29,11 +28,15 @@ select c.first_name, c.last_name
 from customer c 
 where ((select count(*) from rental r2 where r2.customer_id = c.customer_id) > 1);
 
-
-select distinct customer_id 
-from rental
-
-  
+# Otra forma
+SELECT c.customer_id, first_name, last_name
+  FROM rental r1, customer c
+ WHERE c.customer_id = r1.customer_id
+   AND NOT EXISTS (SELECT *
+                     FROM rental r2
+ 		            WHERE r1.customer_id = r2.customer_id
+ 		              AND r1.rental_id <> r2.rental_id);
+ 		             
 #5
 select first_name, last_name
 from actor 
@@ -43,6 +46,7 @@ where actor_id in (select actor.actor_id
  				and fa.film_id = f.film_id 
  				and f.title in ('BETRAYED REAR', 'CATCH AMISTAD'));
  				
+ 			
  			
 #6
 select first_name, last_name
